ebe90138f7cf2654ec3c108fc32717eca7818871
Muskaan Ansari <muskaanansari28@gmail.com>
2022-02-20 00:00:49 +0530

    payment mode integrated

---
+++ PoketApp-master/lib/screens/payment_mode.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shop_app/screens/check_out_screen.dart';
+import 'package:shop_app/screens/app_payment.dart';
+
+import 'package:shop_app/screens/cash_payment.dart';
+
+import 'package:shop_app/constants.dart';
+
+import '../size_config.dart';
+
+class PaymentMode extends StatelessWidget {
+  static const routeName = "paymentmode";
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PAYMENT MODE",
+        style:TextStyle(
+          color: Colors.white,
+          
+        ) 
+        ),
+        backgroundColor: Color(0xFF0D47A1),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "CHOOSE PAYMENT MODE",
+               // style: Theme.of(context).textTheme.bodyText1,
+               //style: headingStyle,
+              style: TextStyle(
+              fontSize: 25,
+                       ),
+                       ),
+             
+             //         SizedBox(height: getProportionateScreenHeight(5)),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 50,
+                          vertical: 50,
+                        ),
+                        child: RaisedButton(
+                          child: Text(
+                            "CASH",
+                            style: TextStyle(
+                              color: Colors.white
+                              
+                            ),
+                          ),
+                          onPressed: (
+                          ) {
+                            Navigator.of(context).pushNamed(CashPay.routeName);
+                          },
+                          color: Color(0xFF0D47A1),
+                         // color: Theme.of(context).accentColor,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 15,
+                          ),
+                        ),
+                      ),
+                 //  SizedBox(height: getProportionateScreenHeight(5)),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 50,
+                          vertical: 50,
+                        ),
+                        child: RaisedButton(
+                          child: Text(
+                            "PayTm",
+                            style: TextStyle(
+                              color: Colors.white
+                              
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(Paytm.routeName);
+                          },
+                          color: Color(0xFF0D47A1),
+                         // color: Theme.of(context).accentColor,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 15,
+                          ),
+                        ),
+                      ),
+                      //SizedBox(height: getProportionateScreenHeight(2)),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 50,
+                          vertical: 50,
+                        ),
+                        child: RaisedButton(
+                          child: Text(
+                            "DEBIT/CREDIT CARD",
+                            style: TextStyle(
+                              color: Colors.white
+                              
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(CheckOutScreen.routeName);
+                          },
+                          color: Color(0xFF0D47A1),
+                         // color: Theme.of(context).accentColor,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 15,
+                          ),
+                        ),
+                      ),
+                   
+                   
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+ 
